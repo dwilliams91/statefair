d8587287e6d115d66b79a7c79baa6ebc7403a647
Your Github User Name <david.lav.williams@gmail.com>
2020-10-21 15:23:46 -0500

    the sideshow ticket button renders the sideshow

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,12 @@ import {TicketBooth} from './TicketBooth.js'
 import {RideTicketHolders} from './rides/RideTicketHolders.js'
 import {foodTicketHolders} from './food/FoodTicketHolder.js'
 import {gameTicketHolders} from './games/GameTicketHolders.js'
+import {sideshowTicketHolders} from './sideshows/sideshowTicketHolder.js'
+
 const eventHub = document.querySelector("#state-fair")
 
 TicketBooth()
 RideTicketHolders()
 foodTicketHolders()
 gameTicketHolders()
+sideshowTicketHolders()
